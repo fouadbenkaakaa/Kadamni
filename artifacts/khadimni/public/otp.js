560e68f2ac0e2868136c3dbf13e38dea13478657
fouadbenkaakaa <fouadbenkaakaa@gmail.com>
2026-08-08 01:29:58 +0100

    Add OTP screen helpers

--- artifacts/khadimni/public/otp.js
+++ artifacts/khadimni/public/otp.js
@@ -1,1 +1,2 @@
-window.KHADIMNI_OTP_READY = true;
+function otpCode(){var s="";for(var i=0;i<6;i++){var e=document.getElementById("otp-"+i);s+=e?e.value:"";}return s;}
+function verifyRegistrationOtp(){alert("OTP");}
