d4a466e4d7345fb7f34d783d97d7ec6cfee741e3
fouadbenkaakaa <fouadbenkaakaa@gmail.com>
2026-08-08 01:30:02 +0100

    Connect OTP client to registration endpoint

--- artifacts/khadimni/public/otp.js
+++ artifacts/khadimni/public/otp.js
@@ -1,2 +1,1 @@
-function otpCode(){var s="";for(var i=0;i<6;i++){var e=document.getElementById("otp-"+i);s+=e?e.value:"";}return s;}
-function verifyRegistrationOtp(){alert("OTP");}
+function requestRegistrationOtp(body){var path="/auth/register/request-otp";return apiPost(path,body);}
